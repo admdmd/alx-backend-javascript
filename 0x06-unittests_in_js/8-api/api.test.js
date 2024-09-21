69d7708749ab0b32ebd634aa7c0911b077db8b21
ademide <ademideadeboye@gmail.com>
2024-09-21 04:02:55 +0100

    test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,10 +5,11 @@ describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+    request.get(`${API_URL}/`, (err, res, body) => {
+      expect(res).to.have.property('statusCode', 200);
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 });
+
